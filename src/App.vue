8b1de137f6e0c7babaa0f28fa9f14ecaa5e8ca62
chunchia.liang <chunchia.liang61@titansoft.com.sg>
2022-11-25 09:02:24 +0800

    updata

---
+++ src/App.vue
@@ -0,0 +1,418 @@
+<template>
+  <div class="grid grid-cols-12 w-100 px-2">
+    <div class="col-span-9">
+      <div 
+      v-for="(week, index) in weekNum" :key="'week' + index" 
+      ref="weekBoard"
+      class="my-2 border-2 border-black"
+      @drop="onDrop(week)"
+      @dragover.prevent
+      @dragenter.prevent
+      >
+        <h2>{{ week }}</h2>
+        <div class="flex flex-wrap">
+          <div v-for="(data, i) in beShowedData[week]" :key="data.className + i"
+        >
+          <ShowCard 
+          :card-data="data"
+          :draggable="isDragAble"
+          @dragstart="startDrag($event,data,i,week)"
+          @dragover.prevent
+          @dragenter="dropEnterMove(i)"
+          />
+        </div>
+        </div>
+      </div>
+    </div>
+
+    <div 
+    class="col-span-3 border-2 border-black ml-1 my-2 bg-purple-400"
+    ref="pileBoard"
+    @drop="onDrop(week)"
+    @dragover.prevent
+    @dragenter.prevent
+    >
+    <h2>pile</h2>
+      <div v-for="(data, i) in pileData" :key="data.className + i">
+        <ShowCard 
+        :card-data="data"
+        :draggable="isDragAble"
+        @dragstart="startDrag($event,data,i,week)"
+        @dragover.prevent
+        @dragenter="dropEnterMove(i)"
+        />
+      </div> 
+      
+    </div>
+  </div>
+
+
+</template>
+
+
+<script setup>
+import { ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
+import ShowCard from "./components/ShowCard.vue";
+const pileData = ref([
+  {
+    className: 'refactor',
+    classType: 'Technical',
+    classDate: '',
+    speaker: ['Bell'],
+    isClassDone: false,
+    isSKADone: false,
+    isPile: true,
+  },
+  {
+    className: 'MVC',
+    classType: 'Technical',
+    classDate: '',
+    speaker: ['Bell', 'cindy'],
+    isClassDone: false,
+    isSKADone: false,
+    isPile: true,
+  }
+])
+const beShowedData =ref( {
+  week1: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+  week2: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+  week3: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+  week4: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+  week5: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+  week6: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+  week7: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+  week8: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+  week9: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+  week10: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+  week11: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+  week12: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+  week13: [
+    {
+      className: 'refactor',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    },
+    {
+      className: 'MVC',
+      classType: 'Technical',
+      classDate: '',
+      speaker: ['Bell', 'cindy'],
+      isClassDone: false,
+      isSKADone: false,
+      isPile: false,
+    }
+  ],
+})
+const isDragAble = ref(true);
+const weekNum = ref(Object.keys(beShowedData.value));
+let tempData=
+  { tempInfo:{},
+    tempWeek:null,
+    tempIndex:null
+};
+let dropIndex = null;
+function startDrag(e,data,index,week){
+  
+  e.dataTransfer.dropEffect = 'move';
+  e.dataTransfer.effectAllowed = 'move';
+  tempData.tempInfo = data;
+  tempData.tempWeek = week;
+  tempData.tempIndex = index;
+  
+}
+function writeDropIndex(index){
+  dropIndex = index;
+}
+function dropEnterMove(index){
+  writeDropIndex(index);
+}
+function spliceDragData(){
+  if(tempData.tempWeek){
+    beShowedData.value[tempData.tempWeek].splice(tempData.tempIndex,1);
+  } else{
+    pileData.value.splice(tempData.tempIndex,1);
+  }
+}
+function writeDragData(week){
+  if(week){
+    tempData.tempInfo.isPile = false;
+    beShowedData.value[week].splice(dropIndex,0,tempData.tempInfo);
+  } else{
+    tempData.tempInfo.isPile = true;
+    pileData.value.splice(dropIndex,0,tempData.tempInfo);
+  }
+}
+function onDrop(week){
+  spliceDragData();
+  writeDragData(week);
+  tempData = { 
+    tempInfo:{},
+    tempWeek:null,
+    tempIndex:null
+};
+  dropIndex = null;
+  saveDataToLoclStorage()
+}
+function saveDataToLoclStorage(){
+  localStorage.setItem('pileData',JSON.stringify(pileData.value));
+  localStorage.setItem('beShowedData',JSON.stringify(beShowedData.value));
+}
+function checkLocaStorageAndReplace(){
+  if(!JSON.parse(localStorage.getItem('pileData'))){
+    localStorage.setItem('pileData',JSON.stringify(pileData.value));
+  }else{
+    pileData.value = JSON.parse(localStorage.getItem('pileData'));
+  }
+  if(!JSON.parse(localStorage.getItem('beShowedData'))){
+    localStorage.setItem('beShowedData',JSON.stringify(beShowedData.value));
+  }else{
+    beShowedData.value = JSON.parse(localStorage.getItem('beShowedData'));
+  }
+}
+checkLocaStorageAndReplace();
+
+onMounted(() => {
+
+})
+</script>
+
+
+
+<style scoped>
+.bg-active {
+    opacity: 0.7;
+}
+</style>
---
+++ src/components/ShowCard.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div 
+    class="border-2 border-black rounded-lg m-1 p-2 w-auto"
+    :class="{ 'bg-slate-200': true }">
+        <h2>{{ props.cardData.className }}</h2>
+        <h2>{{ props.cardData.classType }}</h2>
+        <div class="flex gap-1">
+            <h2 class="flex gap-1 " v-for="speaker in cardData.speaker" :key="speaker">{{ speaker }}</h2>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(["cardData"])
+</script>
